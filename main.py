2075f9330bef76b605174c705d6da40ecb0ae742
888-coding <pachelee@gmail.com>
2025-07-13 00:22:41 -0300

    做了主要main,還有start screen

--- main.py
+++ main.py
@@ -2,10 +2,18 @@
 import curses 
 from curses import wrapper
 
-def main(stdscr):
+def start_screen(stdscr):
     stdscr.clear()
-    stdscr.addstr("Hello world!")
+    stdscr.addstr("Welcome to the speed typing test!")
+    stdscr.addstr("\nPress any key to begin!")
     stdscr.refresh()
     stdscr.getkey()
+
+def main(stdscr):
+    curses.init_pair(1, curses.COLOR_GREEN, curses.COLOR_WHITE)
+    curses.init_pair(2, curses.COLOR_RED, curses.COLOR_BLACK)
+    curses.init_pair(3, curses.COLOR_WHITE, curses.COLOR_BLACK)
+
+    start_screen(stdscr)
 
 wrapper(main)
