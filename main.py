d9c83c87927d6c0a9cd3b46d84f1d45d22ca0762
888-coding <pachelee@gmail.com>
2025-07-12 23:59:34 -0300

    Hello World

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
 # main.py
+import curses 
+from curses import wrapper
+
+def main(stdscr):
+    stdscr.clear()
+    stdscr.addstr("Hello world!")
+    stdscr.refresh()
+    stdscr.getkey()
+
+wrapper(main)
