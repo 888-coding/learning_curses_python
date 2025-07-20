66f423677875b8db171c3d41fc5c5e29a1994e55
888-coding <pachelee@gmail.com>
2025-07-19 23:31:43 -0300

    tutorial4.py 學習user input 和textboxes

--- tutorial4.py
+++ tutorial4.py
@@ -1,1 +1,29 @@
 # tutorial4 , users input and textboxes
+import curses 
+from curses import wrapper
+import time 
+
+def main(stdscr):
+	curses.init_pair(1, curses.COLOR_BLUE, curses.COLOR_YELLOW)
+	curses.init_pair(2, curses.COLOR_GREEN, curses.COLOR_BLACK)
+	curses.init_pair(3, curses.COLOR_RED, curses.COLOR_BLACK)
+
+	x, y = 0, 0 
+
+	while True:
+		key = stdscr.getkey()
+		if key == "KEY_LEFT":
+			x -= 1
+		elif key == "KEY_RIGHT":
+			x += 1 
+		elif key == "KEY_UP":
+			y -= 1 
+		elif key == "KEY_DOWN":
+			y += 1 
+
+		stdscr.clear()
+		stdscr.addstr(y, x,"0")
+		stdscr.refresh()		
+
+
+wrapper(main)
